0d6a1c32a8e0895b99614fab59385ee5db653aa5
Jonathan Strickland <jonathanstrickland@Jonathans-Mac-mini.local>
2021-09-27 18:22:23 -0400

    init commit // finished ex24

---
+++ src/main/java/Runner.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Jonathan Strickland
+ */
+
+import ex24.App;
+import java.util.Scanner;
+
+/**
+ * Main class is used to run examples
+ */
+public class Runner {
+    public static void main(String[] args) {
+       ex24.App.main(args);
+    }
+}
---
+++ src/main/java/ex24/App.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Jonathan Strickland
+ */
+package ex24;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Anagram checker
+ */
+public class App {
+    public static void main(String [] args) {
+        try (Scanner scan = new Scanner(System.in)){
+            String s1 = "";
+            String s2 = "";
+            System.out.println("Enter two strings and I'll tell if you if they are anagrams: ");
+            System.out.print("Enter your first string: ");
+            s1 = scan.nextLine();
+            System.out.print("Enter your second string: ");
+            s2 = scan.nextLine();
+            System.out.println("\"" + s1 + "\"" + " and \"" + s2 + "\" are" + (isAnagram(s1, s2) ? " " : " not " ) + "anagrams.");
+        }
+    }
+
+    /**
+     * Returns true if s1 and s2 are anagrams.
+     * @param s1 string 1
+     * @param s2 string 2
+     */
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        // check to see if every char in s1 is in s2.
+        for (String s : s1.split("")) {
+            if (!s2.contains(s))
+                return false;
+        }
+        return true;
+    }
+}
