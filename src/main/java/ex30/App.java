35d95cd98f7b8ebb71403f48693d5bdc59a66ad2
Jonathan Strickland <jonathanstrickland@Jonathans-Mac-mini.local>
2021-09-27 19:48:32 -0400

    finished assignment 30

---
+++ src/main/java/ex30/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Jonathan Strickland
+ */
+package ex30;
+
+public class App {
+    private static final int tableSize = 12;
+    public static String run() {
+        String returnTable = "";
+        // compute multiplication table
+        for (int row = 1; row < tableSize + 1; row++) {
+            int sum = 0;
+            for (int col = 1; col < tableSize + 1; col++) {
+                sum += row;
+                returnTable += sum + "\t";
+            }
+            returnTable += "\n";
+        }
+        return returnTable;
+    }
+}
